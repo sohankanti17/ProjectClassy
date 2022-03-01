b212198c3c1236cdfe91df80f8692b5a51857b5f
Akshayky26 <akshayky26@gmail.com>
2022-03-01 23:50:37 +0530

    New Nav

--- partials/navbar_partial.php
+++ partials/navbar_partial.php
@@ -1,31 +1,8 @@
 <!-- Navbar html will go here -->
 
-<link
-      rel="stylesheet"
-      href="assets\css\styles.css"
-    />
+<link rel="stylesheet"   href="assets\css\styles.css"/> 
 
-<nav>
-<div class="container">
-
-<h1>Classy Legal</h1>
-<div class="menu">
-    <a href="#" class="is-active">Home</a>
-    <a href="#">Start Your Business</a>
-    <a href="#">GST</a>
-    <a href="#">Income Tax</a>
-    <a href="#">Compalience</a>
-    <a href="#">Documentation</a>
-    <a href="#">Contact Us</a>
+<div class="nav">
+    <div class="logo"><img class="i" src="assets\images\logo.png" class="img-fluid" alt="Logo"></div>
+    <div class="items">Home</div>
 </div>
-
-<button class="burger">
-    <span></span>
-    <span></span>
-    <span></span>
-</button>
-
-</div>
-</nav>
-
-<script src="assets\js\script.js"></script>
