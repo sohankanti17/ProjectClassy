e900a3ced46f315b825ee21ae6d19a9b8ad0716a
Sohan Kanti <30533599+sohankanti17@users.noreply.github.com>
2022-03-01 22:40:19 +0530

    Added footer

--- partials/footer_partial.php
+++ partials/footer_partial.php
@@ -1,3 +1,58 @@
-<footer>
-    
-</footer>
+<div class="container border-top mt-5">
+  <footer class="py-5">
+    <div class="row gy-3">
+      <div class="col-12 col-md-3">
+        <h5 class="mb-3">Start your business</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Private limited company</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Limited liability partner</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">One person company</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Partnership firm</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Business plan</a></li>
+        </ul>
+      </div>
+
+      <div class="col-12 col-md-3">
+        <h5 class="mb-3">Brand protection</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Trademark filling</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Copyright filling</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Patent filling</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Trademark objection</a></li>
+        </ul>
+      </div>
+
+      <div class="col-12 col-md-3">
+        <h5 class="mb-3">Taxation and support</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">GST registration</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FSSAI registration</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Import export code</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Income tax filling</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Service tax registration</a></li>
+        </ul>
+      </div>
+
+      <div class="col-12 col-md-3">
+        <h5 class="mb-3">Company compliances</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Add a additional director</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Annual compliances of LLP</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Annual compliances of company</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Increase of authorized capital</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Removal of director</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-between pt-4 mt-5 border-top">
+      <p>&copy; 2021 Classy Legal. All rights reserved.</p>
+      <ul class="list-unstyled d-flex">
+        <li class="me-2"><a href="#" class="link-dark"><i class="fa fa-twitter fa-lg"></i></a></li>
+        <li class="me-2"><a href="#" class="link-dark"><i class="fa fa-instagram fa-lg"></i></a></li>
+        <li class="me-2"><a href="#" class="link-dark"><i class="fa fa-facebook-square fa-lg"></i></a></li>
+        <li class="me-2"><a href="#" class="link-dark"><i class="fa fa-linkedin fa-lg"></i></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
