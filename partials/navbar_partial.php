a41e61365e804efae8b0ab01f694a486be35b47b
Akshayky26 <akshayky26@gmail.com>
2022-03-02 11:51:29 +0530

    Conflict Cleared

--- partials/navbar_partial.php
+++ partials/navbar_partial.php
@@ -1,9 +1,9 @@
-<!-- Navbar html will go here -->
+<!-- Navbar html goes here -->
 
 <link rel="stylesheet"   href="assets\css\styles.css"/> 
 
 <div class="nav">
-    <div class="logo"><img class="i" src="assets\images\logo.png" class="img-fluid" alt="Logo"></div>
+    <div class="logo"><img class="img" src="assets\images\logo.png" class="img-fluid" alt="Logo"></div>
     <div class="items">Home</div>
 </div>
 
