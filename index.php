e823722bf3ea43469de48c3cb14f09f0718798f4
Akshayky26 <akshayky26@gmail.com>
2022-03-02 20:29:08 +0530

    Navbar Done
    Without styling

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
         <!-- Any page specific styling will go here -->
         <style type="text/css"></style>
+        
     </head>
     <body>
         <!-- Navigation bar -->
@@ -18,6 +19,15 @@
         
         <!-- Actual page content starts from here -->
 
+
+        <div class="container " >
+            
+                    <h1>Helped over 5,000+ Entrepreneurs<br> to launch their startups</h1><br>
+                    <h2>India's largest platform in legal services</h2><br><br><br><br>
+                    <h4>Search your company/CIN</h4><br><br><br>
+                    <p>Most Trending Services:</p>
+
+        </div>
 
 
         <!-- Footer -->
@@ -28,5 +38,6 @@
 
         <!-- Any page specific script will go here -->
         <script type="text/javascript"></script>
+
     </body>
 </html>
---
+++ partials/navbar_partial.php
@@ -0,0 +1,58 @@
+<nav class="navbar shadow-sm p-3 mb-5 bg-body rounded">
+    <div class="container">
+        <a class="navbar-brand">
+            <img src="assets\images\logo.png" alt="Logo" width="130" height="35">
+        </a>
+        <div class="btn-group">
+         <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">Fund Raising</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">Documentation</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Reaction</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">Compliance</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">Income Tax</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">GST & Accounting</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">TradeMark & IP</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+            <button type="button" class="btn " data-bs-toggle="dropdown" aria-expanded="false">Start Your Business</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
