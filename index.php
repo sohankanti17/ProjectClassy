06388a5c71220eed58e5818217ab51c4cdf45901
Akshayky26 <akshayky26@gmail.com>
2022-03-07 19:20:26 +0530

    Main page

--- index.php
+++ index.php
@@ -11,19 +11,22 @@
 
         <!-- Any page specific styling will go here -->
         <style type="text/css"></style>
+
         
     </head>
-    <body>
+    <body >
         <!-- Navigation bar -->
         <?php require_once('./partials/navbar_partial.php') ?>
         
         <!-- Actual page content starts from here -->
 
 
-        <div class="fluid-container d-flex justify-content-center align-items-center" style="height:95vh">
-            <div class="container col-lg-6 px-4 mx-auto text-center">
-
-                <h1 class="display-6 fw-bold"> Helped over 5,000+ Entrepreneurs to launch their startups</h1><br>
+        <div class=" fluid-container" style="height:95vh; background-image:url('.\\assets\\images\\bg1.png');background-repeat: no-repeat;
+    background-size: cover; padding-top:250px">
+            <!-- <img src="./assets\images\bg1.png" alt="" style="height:200px"> -->
+            <div class="container col-lg-6 px-4 mx-auto text-center ">
+                
+                <h1 class="display-6 fw-bold "> Helped over 5,000+ Entrepreneurs to launch their startups</h1><br>
                 <p class="lead mb-4">
                     Marvel's The Avengers classified under the name Marvel Avengers Assemble in the United Kingdom 
                     and Ireland,or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics
@@ -136,6 +139,27 @@
     </div>
 
 </div>
+
+
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+        <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+          <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
 
 
 
--- partials/styles_partial.php
+++ partials/styles_partial.php
@@ -1,4 +1,4 @@
-<link rel="shortcut icon" href="LOGO" type="image/x-icon">
+<!-- <link rel="shortcut icon" href="LOGO" type="image/x-icon"> -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
